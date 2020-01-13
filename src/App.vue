3ae3b9d20d00bf3a84c4c644036ab749b155b73f
Yannis Constantin <yanniskonst@hotmail.fr>
2020-01-13 18:06:40 -0500

    Main page, list of connections + people

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,30 @@
 </template>
 
 <script>
-import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+  import Vue from 'vue'
+  import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+  Vue.use(BootstrapVue)
+  Vue.use(IconsPlugin)
 
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
 
-
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld
+    }
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,67 @@
 <template>
   <div>
-    <b-table striped hover :items="items" :fields="fields"></b-table>
+    <div>
+      <span>List of connections</span>
+      <b-table striped hover :items="connections" :fields="connections_fields"></b-table>
+    </div>
+    <div>
+      <span>List of connections</span>
+      <b-table striped hover :items="people" :fields="people_fields"></b-table>
+    </div>
   </div>
 </template>
 
 <script>
+const BASE_URL = process.env.VUE_APP_BASE_URL;
+const CONNECTIONS_END_POINT = '/connections';
+const PEOPLE_END_POINT = '/people';
+import axios from 'axios';
   export default {
     data() {
       return {
         // Note `isActive` is left out and will not appear in the rendered table
-        fields: ['first_name', 'last_name', 'age'],
-        items: [
-          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
+        connections_fields: ['id', 'connection_type', 'from_person_id', 'to_person_id'],
+        people_fields: ['id', 'email', 'first_name', 'last_name'],
+        connections: [],
+        people: [],
+        error: ''
       }
+    },
+    mounted() {
+      axios.get(BASE_URL + CONNECTIONS_END_POINT).then(response => (
+        this.connections = response.data
+      )).catch(error => {
+        this.errorMsg = 'No user or no location!'
+        this.error = error
+        this.data = []
+      })
+      axios.get(BASE_URL + PEOPLE_END_POINT).then(response => (
+        this.people = response.data
+      )).catch(error => {
+        this.errorMsg = 'No user or no location!'
+        this.error = error
+        this.data = []
+      })
     }
-  }
+}
+  
 </script>
 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
